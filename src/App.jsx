b6b56f24204f66cdc6fc27d9b01aac544ad93327
AtanasovCode <a_ljupce@yahoo.com>
2024-01-21 22:30:52 +0100

    created global styles and dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import styled from "styled-components";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 
 const App = () => {
+
+  const darkTheme = {
+    background: "#0A1C1C",
+    primary: "#BECCFF",
+    secondary: "#64686C",
+    text: "#CECECE",
+    accent: "#DE6100",
+
+    font: "",
+  }
+
   return (
-    <Container></Container>
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <Container></Container>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const Container = styled.div`
+  min-height: 100vh;
+  background-color: ${props => props.theme.background};
+  font-family: ${props => props.theme.font};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
